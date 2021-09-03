0007744ae9c441e8ce56d82183a5131bf3dc6b1b
anupprakash4 <anupprakash4@gmail.com>
2021-09-03 18:53:27 +0530

    Tag for version 2

--- automation.sh
+++ automation.sh
@@ -8,49 +8,49 @@
    run_sudo=$(sudo apt update -y)
    echo "$run_sudo"
 
-   if [ $(dpkg-query -W -f='${Status}' apache2 2>/dev/null | grep -c "ok installed") -eq 0 ];
-   then
+    if [ $(dpkg-query -W -f='${Status}' apache2 2>/dev/null | grep -c "ok installed") -eq 0 ];
+    then
       sudo apt install -y apache2
-   else 
+    else 
       echo "Package already installed"	  
-   fi
+    fi
  
 package_check_awscli=`apt -qq list awscli --installed |wc -l`
 
-   if [ $package_check_awscli == 0 ]
-   then
-     apt-get install awscli -y
-   else
-     echo "AWS CLI installed already"  
-   fi
+    if [ $package_check_awscli == 0 ]
+    then
+      apt-get install awscli -y
+    else
+      echo "AWS CLI installed already"  
+    fi
 
 #check Apache running status 
-   if pgrep -x "apache2" >/dev/null
-   then 
-     echo "apache2 is running"
-   else
-     service apache2 start
-   fi  
+    if pgrep -x "apache2" >/dev/null
+    then 
+      echo "apache2 is running"
+    else
+      service apache2 start
+    fi  
 
 #check apache enabled or not
 apache_check=`systemctl status apache2.service  | grep Active | awk '{ print $3 }'`
 
-  if [ $apache_check == "(dead)" ]
-  then
-    systemctl enable apache2.service
-  fi
+   if [ $apache_check == "(dead)" ]
+   then
+     systemctl enable apache2.service
+   fi
 
-  timestamp="$(date '+%d%m%Y-%H%M%S')"
-  filename="/tmp/${myname}-httpd-logs-${timestamp}.tar"
+   timestamp="$(date '+%d%m%Y-%H%M%S')"
+   filename="/tmp/${myname}-httpd-logs-${timestamp}.tar"
 
 #create tar file
-  tar -cvf ${filename} $( find /var/log/apache2/ -name "*.log")
+   tar -cvf ${filename} $( find /var/log/apache2/ -name "*.log")
 
-  filesize=$(du -sh $filename | awk '{print $1}')
+   filesize=$(du -sh $filename | awk '{print $1}')
 
 #Copying to AWS S3 bucket
 
-  aws s3 cp ${filename} s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
+   aws s3 cp ${filename} s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
 
   
 #Task 3 - To keep logs in inventory.html
