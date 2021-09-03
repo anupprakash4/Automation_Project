699cf971b2c5b55c41aa596a0210254fe791f7f4
anupprakash4 <anupprakash4@gmail.com>
2021-09-03 15:12:35 +0530

    my first bash Script

--- automation.sh
+++ automation.sh
@@ -11,18 +11,18 @@ echo "Package Updates"
 sudo apt-get update -y
 if [ $(dpkg-query -W -f='${Status}' apache2 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-	  sudo apt install -y apache2
-  else 
-          echo "Package already installed"	  
+    sudo apt install -y apache2
+else 
+    echo "Package already installed"	  
 fi
   
 package_check_awscli=`apt -qq list awscli --installed |wc -l`
 
   if [ $package_check_awscli == 0 ]
   then
-        apt-get install awscli -y
+       apt-get install awscli -y
   else
-        echo "AWS CLI installed already"  
+       echo "AWS CLI installed already"  
   fi
 
 #check Apache running status 
@@ -30,7 +30,7 @@ if pgrep -x "apache2" >/dev/null
 then 
    echo "apache2 is running"
 else
-    service apache2 start
+   service apache2 start
 fi  
 
 #check apache enabled or not
@@ -38,7 +38,7 @@ apache_check=`systemctl status apache2.service  | grep Active | awk '{ print $3 
 
 if [ $apache_check == "(dead)" ]
 then
-     systemctl enable apache2.service
+    systemctl enable apache2.service
 fi
 
 #create tar file
@@ -49,38 +49,4 @@ filesize=$(du -sh $filename | awk '{print $1}')
 #Copying to AWS S3 bucket
 
 aws s3 cp ${filename} s3://${s3_bucket}/${filename}
-
-#Task 3 - To keep logs in inventory.html
-	if [ -e /var/www/html/inventory.html ]
-	then
-	    echo "httpd-logs &nbsp;&nbsp;&nbsp; ${timestamp} &nbsp;&nbsp;&nbsp; tar &nbsp;&nbsp;&nbsp; $filesize " >> /var/www/html/inventory.html
-	else
-	    echo "Log Type &nbsp;&nbsp;&nbsp;  Date Created &nbsp;&nbsp;&nbsp; Type &nbsp;&nbsp;&nbsp;  Size<br>" >> /var/www/html/inventory.html
-	    echo "httpd-logs &nbsp;&nbsp;&nbsp; ${timestamp} &nbsp;&nbsp;&nbsp; tar &nbsp;&nbsp;&nbsp; $filesize" >> /var/www/html/inventory.html
-	fi
-
-# check cron file is exist of not, if it is doesn't exist then create it
-# Note:- script will execute once in day at 3.30AM
-if  [ ! -f  /etc/cron.d/automation ]
-then
-   echo "5 4 * * * root /root/Automation_Project/automation.sh" > /etc/cron.d/automation
-fi
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
